1a418491eb813b3d977f16e9941e57e718830e02
Raymeizad Gunaldi <raymeizadgnd@gmail.com>
2026-04-28 10:48:13 +0800

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/kendaraan/index.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+
+   <h1 class="fw-bold">Data Kendaraan</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/kendaraan', function () {
+    return view('kendaraan.index', ['title' => 'Kendaraan']);
+});
+
+Route::get('/kendaraan/create', function () {
+    return view('kendaraan.create', ['title' => 'Create Kendaraan']);
 });
