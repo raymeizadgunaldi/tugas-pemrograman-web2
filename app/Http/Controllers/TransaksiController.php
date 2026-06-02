0f78482a9b5092656377208fff02b0af8efff741
Raymeizad Gunaldi <raymeizadgnd@gmail.com>
2026-06-02 15:29:37 +0800

    feat: implementasi form tambah data traksaksi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Merek;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
@@ -25,15 +26,39 @@ class TransaksiController extends Controller
     {
            return view('transaksi.create', [
             'title' => 'Create Transaksi',
+            'mereks' => Merek::latest()->get(),
             ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+     public function store(Request $request)
     {
-        //
+            $validated = $request->validate([
+       'merek_id'          => 'required|exists:mereks,id',
+        'name'              => 'required|max:255',
+        'tanggal_transaksi' => 'required',
+        'durasi'            => 'required|integer',
+        'harga'             => 'required|integer',
+        'status'            => 'required|in:Pending,Selesai,Dibatalkan',
+    ], [
+        'merek_id.required'          => 'Merek tidak boleh kosong',
+        'merek_id.exists'            => 'Merek yang dipilih tidak ditemukan',
+        'name.required'              => 'Nama tidak boleh kosong',
+        'name.max'                   => 'Nama tidak boleh lebih dari :max karakter',
+        'tanggal_transaksi.required' => 'Tanggal transaksi tidak boleh kosong',
+        'durasi.required'            => 'Durasi tidak boleh kosong',
+        'durasi.integer'             => 'Durasi harus berupa angka',
+        'harga.required'             => 'Harga tidak boleh kosong',
+        'harga.integer'              => 'Harga harus berupa angka',
+        'status.required'            => 'Status tidak boleh kosong',
+        'status.in'                  => 'Status tidak valid',
+       
+    ]);
+
+      Transaksi::create($validated);
+      return to_route('transaksi.index')->withSuccess('data berhasil ditambahkan');
     }
 
     /**
